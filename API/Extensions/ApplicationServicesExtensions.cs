f1cdcada2c442886a87866f74fd18f136826d7d0
risercable <jerald.mark.buljatin@gmail.com>
2023-04-11 16:37:35 +0800

    Added Angular bits 4-11-23

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -42,7 +42,7 @@ public static class ApplicationServicesExtensions
         {
             opt.AddPolicy("CorsPolicy", policy =>
             {
-                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
             });
         });
         
